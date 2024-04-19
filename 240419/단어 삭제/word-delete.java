41c9af3383a5e1af4a4ecb8390cbba334a2e187d
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-04-19 13:18:54 +0900

    Title: 단어 삭제, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 240419/단어 삭제/word-delete.java
+++ 240419/단어 삭제/word-delete.java
@@ -1,12 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        String word = sc.next();
-        String remove_word = sc.next();
-        while(word.contains(remove_word))
-        word = word.replaceAll(remove_word, "");
-        System.out.print(word);
+
+        // 주 문자열과 부 문자열을 선언합니다.
+        String mainStr, subStr;
+        
+        // 문자열을 입력받습니다.
+        mainStr = sc.next();
+        subStr = sc.next();
+
+        // 부 문자열이 주 문자열 내에 존재하는 동안 반복하여 삭제합니다.
+        while (true) {
+            int foundIdx = mainStr.indexOf(subStr); // 부 문자열의 시작 인덱스를 찾습니다.
+
+            // 만약 부 문자열을 찾지 못하면 반복문을 종료합니다.
+            if (foundIdx == -1) {
+                break;
+            }
+
+            // 부 문자열을 주 문자열에서 삭제합니다.
+            mainStr = mainStr.substring(0, foundIdx) + mainStr.substring(foundIdx + subStr.length());
+        }
+
+        // 부 문자열이 모두 삭제된 주 문자열을 출력합니다.
+        System.out.println(mainStr);
     }
 }
