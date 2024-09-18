6c515ef91dc1c5162655c6580d80aa18c4586f4b
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-09-18 11:21:11 +0900

    Title: 삼항연산자 2, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240918/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        print(a==1?"t":"f");
+        
+    }
+    public static <T> void print(T value){
+            System.out.println(value);
+    }
+
+     public static <T> void printf(T value){
+            System.out.printf("%.2f",value);
+    }
+}
