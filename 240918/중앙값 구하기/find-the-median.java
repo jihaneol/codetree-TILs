2af461d67a44bcf14403cb901d9c8eaca349cb3b
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-09-18 11:43:02 +0900

    Title: 중앙값 구하기, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240918/중앙값 구하기/find-the-median.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for(int i=0; i<3; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<2; i++){
+            for(int j=i; j<2; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        print(arr[1]);
+      
+        
+    }
+    public static <T> void print(T value){
+            System.out.println(value);
+    }
+
+     public static <T> void printf(T value){
+            System.out.printf("%.2f",value);
+    }
+}
