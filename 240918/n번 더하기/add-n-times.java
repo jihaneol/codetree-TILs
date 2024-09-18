474dae093ac7c387582751424615af4cdcced4eb
jihaneol <104291422+jihaneol@users.noreply.github.com>
2024-09-18 11:53:27 +0900

    Title: n번 더하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240918/n번 더하기/add-n-times.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            println(a+=n);
+        }
+        // int[] arr = new int[3];
+        // for(int i=0; i<3; i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        // for(int i=0; i<2; i++){
+        //     for(int j=i; j<2; j++){
+        //         if(arr[j]>arr[j+1]){
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
+        // print(arr[1]);
+      
+        
+    }
+    public static <T> void print(T value){
+            System.out.print(value);
+    }
+
+    public static <T> void println(T value){
+            System.out.println(value);
+    }
+
+     public static <T> void printf(T value){
+            System.out.printf("%.2f",value);
+    }
+}
